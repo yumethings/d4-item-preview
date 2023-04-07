b0d904fda96b83027701d2ae5ed919f5afc7e5a4
yumethings <yume.himemiya.twitch@gmail.com>
2023-04-07 19:16:04 +0200

    Remove unused type guard

--- src/utilities/typeGuards.ts
+++ src/utilities/typeGuards.ts
@@ -1,9 +1,5 @@
-import { ClassName, ItemType, ItemValue, UniqueItem } from "../types/types";
+import { ItemValue, UniqueItem } from "../types/types";
 
 export const isUniqueItem = (item: ItemValue): item is UniqueItem => {
 	return "affixes" in item && "flavor" in item;
-};
-
-export const isClassName = (item: ClassName | ItemType): item is ClassName => {
-	return ["Barbarian", "Druid", "Generic", "Necromancer", "Rogue" || "Sorcerer"].includes(item);
 };
