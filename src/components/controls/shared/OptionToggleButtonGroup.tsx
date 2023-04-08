54e8eaf797fffac3d1da0f2d277506cc2fd4a3c2
yumethings <yume.himemiya.twitch@gmail.com>
2023-04-08 03:36:34 +0200

    Make it possible to pass ToggleButtonGroupProps

--- src/components/controls/shared/OptionToggleButtonGroup.tsx
+++ src/components/controls/shared/OptionToggleButtonGroup.tsx
@@ -1,17 +1,19 @@
-import { ToggleButtonGroup, ToggleButton } from "@mui/material";
+import { ToggleButtonGroup, ToggleButton, ToggleButtonGroupProps } from "@mui/material";
 
 interface OptionToggleButtonGroupProps<T extends string> {
 	value: T;
 	options: T[];
 	onChange: (_e: React.MouseEvent<HTMLElement, MouseEvent>, newValue: T) => void;
+	toggleButtonGroupProps?: ToggleButtonGroupProps;
 }
 
 export const OptionToggleButtonGroup = <T extends string>({
 	value,
 	options,
 	onChange,
+	toggleButtonGroupProps,
 }: OptionToggleButtonGroupProps<T>) => (
-	<ToggleButtonGroup color="primary" exclusive value={value} onChange={onChange}>
+	<ToggleButtonGroup color="primary" exclusive value={value} onChange={onChange} {...toggleButtonGroupProps}>
 		{options.map((option, idx) => (
 			<ToggleButton key={idx} value={option}>
 				{option}
